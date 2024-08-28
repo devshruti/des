0291d2e1a14bb2ffb28bb946e38608721f89f06d
unknown <sendevshruti@gmail.com>
2024-08-28 20:35:47 +0530

    first commit

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const express=require("express")
-const{userRouter}=require("./router/userRouter")
-const{productRouter}=require("./router/productRouter")
-const {authMiddleware}=require("./middleware/authenticate")
+const express = require("express")
+const { userRouter } = require("./router/userRouter")
+const { productRouter } = require("./router/productRouter")
+const { authMiddleware } = require("./middleware/authenticate")
 const { connectedDB } = require("./db")
 
-const app=express()
+const app = express()
 require("dotenv").config()
 
 
- 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/",async(req,res)=>{
-    try{
+app.get("/", async (req, res) => {
+    try {
         res.send("Home-Page")
     }
-    catch(error){
+    catch (error) {
         console.log(`Error:${error}`)
     }
 })
 
-app.use("/user",userRouter)
+app.use("/user", userRouter)
 app.use(authMiddleware)
-app.use("/product",productRouter)
+app.use("/product", productRouter)
 
 
 /* listen the server code present here */
-const port=process.env.port||8080
-app.listen(port,async()=>{
-    try{
+const port = process.env.port || 5000
+app.listen(port, async () => {
+    try {
         //connected db
         await connectedDB
         console.log("Database connected Successfully")
     }
-    catch(err){
+    catch (err) {
         console.log(err.message)
-        }
+    }
     console.log(`server is running on port ${port}`)
 })
